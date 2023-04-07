4cacdb3790e94e74590f785a359605f67ce1c01a
Emanuele <egalli64@gmail.com>
2023-04-07 20:29:36 +0200

    partitioning left vs given pivot position

--- src/main/java/com/example/jaal/s10/Partitioning.java
+++ src/main/java/com/example/jaal/s10/Partitioning.java
@@ -11,23 +11,28 @@ import org.slf4j.LoggerFactory;
 public class Partitioning {
     private static final Logger log = LoggerFactory.getLogger(Partitioning.class);
 
+    /**
+     * Partition the passed array using its first element as pivot
+     * 
+     * @param data a non-empty array
+     * @return the pivot position after partitioning
+     * @throws ArrayIndexOutOfBoundsException empty array
+     * @throws NullPointerException           null array
+     */
     public static int pivotLeft(int[] data) {
         log.trace("Partitioning {}", data);
-        return pivotLeft(data, 0, data.length);
-    }
 
-    private static int pivotLeft(int[] data, int begin, int end) {
-        int pivot = data[begin];
-        int i = begin;
-        int j = end;
+        int pivot = data[0];
+        int i = 0;
+        int j = data.length;
         while (true) {
             while (data[++i] < pivot) {
-                if (i == end) {
+                if (i == data.length) {
                     break;
                 }
             }
             while (data[--j] > pivot) {
-                if (j == begin) {
+                if (j == 0) {
                     break;
                 }
             }
@@ -39,10 +44,34 @@ public class Partitioning {
             swap(data, i, j);
         }
 
-        swap(data, begin, j);
+        swap(data, 0, j);
         return j;
     }
 
+    /**
+     * Partition the passed array using its first element as pivot
+     * 
+     * @param data a non-empty array
+     * @param i    the pivot position
+     * @return the pivot position after partitioning
+     * @throws ArrayIndexOutOfBoundsException bad index
+     * @throws NullPointerException           null array
+     */
+    public static int pivotGiven(int[] data, int i) {
+        log.trace("Partitioning with pivot {} in position {}", data[i], i);
+        swap(data, i, 0);
+        return pivotLeft(data);
+    }
+
+    /**
+     * Swap elements in array
+     * <p>
+     * Preconditions: array is not empty, indexes are "good"
+     * 
+     * @param data the array
+     * @param i    first element position
+     * @param j    second element position
+     */
     private static void swap(int[] data, int i, int j) {
         log.trace("Swapping {} and {}", data[i], data[j]);
 
--- src/test/java/com/example/jaal/s10/PartitioningTest.java
+++ src/test/java/com/example/jaal/s10/PartitioningTest.java
@@ -6,6 +6,8 @@
 package com.example.jaal.s10;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
 import org.junit.jupiter.api.Test;
 
@@ -42,5 +44,70 @@ class PartitioningTest {
         for (int i = actual; i < values.length; i++) {
             assertThat(values[i]).isGreaterThanOrEqualTo(pivot);
         }
+    }
+
+    @Test
+    void pivotLeftNull() {
+        assertThatNullPointerException().isThrownBy(() -> Partitioning.pivotLeft(null));
+    }
+
+    @Test
+    void pivotLeftEmpty() {
+        assertThatExceptionOfType(ArrayIndexOutOfBoundsException.class)
+                .isThrownBy(() -> Partitioning.pivotLeft(new int[] {}));
+    }
+
+    @Test
+    void pivotGivenPlain() {
+        int pivotPos = 4;
+        int pivot = 6;
+        int[] values = { 7, 1, 8, 5, pivot, 0, 9, 3, -1, -3, 18, 10, 2 };
+        int expected = 7;
+
+        int actual = Partitioning.pivotGiven(values, pivotPos);
+        assertThat(actual).isEqualTo(expected);
+        for (int i = 0; i < actual; i++) {
+            assertThat(values[i]).isLessThanOrEqualTo(pivot);
+        }
+
+        for (int i = actual; i < values.length; i++) {
+            assertThat(values[i]).isGreaterThanOrEqualTo(pivot);
+        }
+    }
+
+    @Test
+    void pivotGivenDuplicates() {
+        int pivotPos = 4;
+        int pivot = 6;
+        int[] values = { 7, 1, 8, 5, pivot, 1, 9, 3, 5, 3, 8, 7, 2, 6 };
+        int expected = 8;
+
+        int actual = Partitioning.pivotGiven(values, pivotPos);
+        assertThat(actual).isEqualTo(expected);
+        for (int i = 0; i < actual; i++) {
+            assertThat(values[i]).isLessThanOrEqualTo(pivot);
+        }
+
+        for (int i = actual; i < values.length; i++) {
+            assertThat(values[i]).isGreaterThanOrEqualTo(pivot);
+        }
+    }
+
+    @Test
+    void pivotGivenNull() {
+        assertThatNullPointerException().isThrownBy(() -> Partitioning.pivotGiven(null, 0));
+    }
+
+    @Test
+    void pivotGivenEmpty() {
+        assertThatExceptionOfType(ArrayIndexOutOfBoundsException.class)
+                .isThrownBy(() -> Partitioning.pivotGiven(new int[] {}, 0));
+    }
+    
+
+    @Test
+    void pivotGivenBadPosition() {
+        assertThatExceptionOfType(ArrayIndexOutOfBoundsException.class)
+                .isThrownBy(() -> Partitioning.pivotGiven(new int[] {42}, 42));
     }
 }
